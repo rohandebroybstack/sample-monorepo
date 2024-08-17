bd7ba18bc153791e9c453f57c1071efe20c7723b
Rohan Debroy <rohan.de@browserstack.com>
2024-08-17 21:34:17 +0530

    Add sourcemaps

--- apps/accessibility-toolkit-headless/vite.config.js
+++ apps/accessibility-toolkit-headless/vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig, mergeConfig } from "vite";
 
 import manifest from "./manifest";
 
-
 /** @type {import('vite').UserConfigExport} */
 const bstackProductViteConfig = {
   // FIXME: Excluding react plugin and vendor chunk plugin, find alternative
@@ -27,6 +26,25 @@ const extensionViteConfig = {
       const: path.resolve(__dirname, "src/const"),
       utils: path.resolve(__dirname, "src/utils"),
     },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+          if (/\.css$/.test(name ?? "")) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+    sourcemap: true,
   },
 };
 
